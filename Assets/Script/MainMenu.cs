849caa66e64da69052546a221acb8923d7e85d44
nkanzaki <nkanzaki1621@sdsu.edu>
2025-11-24 12:07:07 -0800

    stages

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Play");   
+    }
+
+    public void OpenGuide()
+    {
+        SceneManager.LoadScene("Guide"); 
+    }
+}
